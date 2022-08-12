554b2f402d047ff863a5c106ed8d3867ca64287b
Matheus Lourenço de Souza <107487181+matheuslsouza@users.noreply.github.com>
2022-08-12 15:32:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operator]');
+const equalButtons = document.querySelector('[data-equal]');
+const deleteButtons = document.querySelector('[data-delete]');
+const previousOperand = document.querySelector('[data-previous-operand]');
+const clearteOperand = document.querySelector ('[data-current-operand]');
+
